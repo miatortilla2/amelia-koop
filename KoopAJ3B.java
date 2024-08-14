af3f865c28f6601dd28fdc05947ae9e2b88d9180
miatortilla2 <155514247+miatortilla2@users.noreply.github.com>
2024-08-14 09:17:07 -0500

    Add files via upload

---
+++ KoopAJ3B.java
@@ -0,0 +1,54 @@
+import java.util.Scanner ; // import Scanner
+
+//Retirement Planning - Java 3B
+//Amelia Koop, Sp24
+
+public class KoopAJ3B
+{  
+   public static void main(String[] args)
+   {
+      double begBalance ; // beginning balance
+      double endBalance ; // ending balance
+      double withdrawlAmt ; // yearly withdrawl amount
+      double earnRate  ; // annual earning rate
+      int yearAccumulator = 1 ; // start at year 1
+      
+      // create scanner object for user input
+      Scanner keyboard = new Scanner(System.in);
+      
+      // ask user beginning balance
+      System.out.println("What is the beginning balance?") ;
+      begBalance = keyboard.nextDouble() ;
+      
+      // ask user for annual withdrawl
+      System.out.println("What is the desired annual withdrawl?") ;
+      withdrawlAmt = keyboard.nextDouble() ;
+      
+      // ask user for earning rate
+      System.out.println("What is the annual earnings rate? (5=5%)") ;
+      earnRate = keyboard.nextDouble() ;
+      
+      // print column headers
+      System.out.println("Year\tBeg Balance\tWithdrawl\tEarnings\tEnd Bal") ;
+      
+      double yearlyEarnings ; // earnings for the year
+      
+      while (yearAccumulator <= 20) // repeat calculation 20 times
+      {
+         yearlyEarnings = ((begBalance - withdrawlAmt) * (earnRate / 100)) ; // calculate yearly earnings
+         endBalance = (begBalance - withdrawlAmt + yearlyEarnings) ; // calculate ending balance
+         
+         // print calculations
+         System.out.println(yearAccumulator + "\t\t" + begBalance + "\t\t" + withdrawlAmt + "\t\t"
+                            + yearlyEarnings + "\t\t" + endBalance) ;
+                            
+         begBalance = endBalance ; // set ending balance to next year's beginning balance
+         
+         ++ yearAccumulator ; // add year to accumulator
+      }
+      
+      System.out.println() ;
+      System.out.println("Above is your projected retirement plan.") ;
+      
+    }
+}
---
+++ KoopAJ3C.java
@@ -0,0 +1,54 @@
+import javax.swing.JOptionPane; // import JOptionPane
+
+//Laminate Flooring Calculator - Java 3C
+//Amelia Koop, Sp24
+
+public class KoopAJ3C
+{  
+   public static void main(String[] args)
+   {
+      String input ; // get user input
+      int numOfRooms ; // number of rooms needing flooring
+      
+      // ask user how many rooms to be floored
+      input = JOptionPane.showInputDialog("How many rooms need flooring installed?") ;
+      numOfRooms = Integer.parseInt(input) ;
+      
+      double totalSquareYds = 0 ; // set accumulator to 0
+      
+      // repeat program for amount of rooms the user requests
+      for (int roomNum = 1; roomNum <= numOfRooms ; roomNum ++)
+      {
+         //ask room length
+         double roomLength = Double.parseDouble(JOptionPane.showInputDialog("Please enter the length of the room: " + roomNum + " (in sq feet):"));
+         
+         //ask room width
+         double roomWidth = Double.parseDouble(JOptionPane.showInputDialog("Please enter the width of the room: " + roomNum + " (in sq feet):"));
+         
+         double areaInFeet = roomLength * roomWidth ; // calculate area in feet
+         double areaInYds = areaInFeet / 9 ; // convert area to yards
+         
+         totalSquareYds += areaInYds ; // add yards to total
+         
+         // print column headers
+         System.out.println("ROOM\tLENGTH\tWIDTH\t\tSQ FT\t\tSQ YDS") ;
+         
+         // print room number, length, width, sq feet, and sq yards
+         System.out.println(roomNum + "\t\t" + roomLength + "\t\t" + roomWidth + "\t\t" + areaInFeet + "\t\t" + areaInYds) ;
+         
+      }
+      
+      // set price per yard
+      double pricePerYd = 23.85 ;
+      
+      System.out.println() ; // blank line
+      System.out.println("\t\t TOTAL SQ YDS: " + totalSquareYds) ; // print total yards
+      System.out.println("\t\t PRICE PER YD: $  " + pricePerYd) ; // print price per yard
+      
+      double totalCost = totalSquareYds * pricePerYd ; // calcuate total cost
+      System.out.println("\t\t TOTAL COST: $  " + totalCost) ; // print total cost
+      
+      System.exit(0) ;
+      
+   }
+}
